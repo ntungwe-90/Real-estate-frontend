00efbe245b5e005a33044f250f9816672fe14d71
Abigail <ashbella333@gmail.com>
2022-12-19 15:16:32 +0000

    added background image and text to home component

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,22 +1,38 @@
 <template>
-    <div class=" h-full">
-        <div>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
-            <h1> Hello World</h1>
+    <div>
+        <!------main image showcase-->
+        <div class="h-screen mx-8 rounded-2xl bg-no-repeat bg-center bg-cover " :style="{ 'background-image': `url(${require('@/static/images/Frame.png')})` }"> 
+            <div class="pl-32 pt-40">
+                <div>
+                    <h1 class="font-mono text-white text-4xl font-extrabold w-1/4"> Modern Urban Apartment with
+                        <span> Modern</span>
+                
+                        Amenities
+                    </h1>
+                </div>
+                <div> 
+                    <img src="images/hero-shape-2.png.png" />
+                </div>
+                
+                <div class="w-2/4 text-white">
+                    <p> Congue nisi, pretium in suspendisse. Enim accumsan ad, ligula pulvinar cursus porttitor nostra. Dictum ipsum
+                        augue,venenatis rhoncus, amet inceptos dolor
+                        venenatis rhoncus, amet inceptos dolor</p>
+                </div>
+
+
+                <div> 
+                    <p class="py-4 w-36 bg-red-600 text-white text-center rounded text-xs"> Learn More</p>
+                </div>
+
+
+
+            </div>
             
-        
+
         </div>
+
+
     </div>
 </template>
 
