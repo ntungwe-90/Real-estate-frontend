04dc2c60cdd61c96900c49fb72a5654e843da3d7
ntungwe-90 <ntongweirene@gmail.com>
2022-12-19 09:27:47 +0000

    added navbar and footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+ 
+  <img src="https://mdbootstrap.com/img/new/slides/041.jpg" class="max-w-full h-auto" alt="..." />
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+ 
 }
 </script>
