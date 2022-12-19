48f6ad8b9885f722d87ecc0f3a3fcd5110f67d2b
Abigail <ashbella333@gmail.com>
2022-12-19 13:38:22 +0000

    created home component

---
+++ pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class=" h-full">
+        <div>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+            <h1> Hello World</h1>
+        
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomePage",
+    layout: "main"
+
+};
+</script>
+
+<style>
+
+</style>
